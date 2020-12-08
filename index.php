7af3c2822fa61fd65d7d67b2dbab681245b5206a
Miguel Gatica <miguel_elias_gatica19@outlook.com>
2020-12-08 13:00:02 -0300

    segunda actualizacion titulo en index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Titulo pagina actualizada</title>
+        <title>Titulo pagina actualizada2</title>
     </head>
     <body>
         <?php
