e39d21220e5bdadf40577151f906ebebb2d33f18
Miguel Gatica <miguel_elias_gatica19@outlook.com>
2020-12-08 11:16:55 -0300

    viendo mi commit en github

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-         echo "HOLA RECORDANDO COMO USAR GIT... SOY USUARIO MIGUEL en 2020"
+         echo "HOLA RECORDANDO COMO USAR GIT... SOY USUARIO MIGUEL en 2020 ..."
         ?>
     </body>
 </html>
